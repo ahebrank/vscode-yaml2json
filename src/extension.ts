e6d2bc940db56ce68f5bdfbb3ab41285c39eb3e8
Andy Hebrank <andy.hebrank@platform.sh>
2024-01-27 19:02:56 -0500

    reindent again

--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,21 @@ import { Command } from './command'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('"yaml2json" is now active')
-	
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	
-	const commands = [
-		vscode.commands.registerCommand(`yaml2json.document`, () => Command.convertDocument()),
-		vscode.commands.registerCommand(`yaml2json.clipboard`, () => Command.convertClipboard()),
-	]
-	
-	context.subscriptions.push(...commands)
+  
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('"yaml2json" is now active')
+  
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  
+  const commands = [
+    vscode.commands.registerCommand(`yaml2json.document`, () => Command.convertDocument()),
+    vscode.commands.registerCommand(`yaml2json.clipboard`, () => Command.convertClipboard()),
+  ]
+  
+  context.subscriptions.push(...commands)
 }
 
 // this method is called when your extension is deactivated
